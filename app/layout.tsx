e6fdca5a3f7c46e24b79fe6843b1d6a2d1d3cc44
stonecast <arunkarki721@gmail.com>
2025-04-28 10:23:43 +0545

    Removed local font from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,23 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 
-const bebasNeue = localFont({
-  src: [
-    { path: '/fonts/bebasNeue-Regular.ttf', weight: '400', style: 'normal' },
-  ],
-  variable: "--bebas-neue",
-});
-
 export const metadata: Metadata = {
-  title: "Movie List",
-  description: "List of watched movies and series.",
+    title: "Movie List",
+    description: "List of watched movies and series.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
