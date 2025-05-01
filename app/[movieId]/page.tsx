437b53e1394cd8c01721e18afda0052f3845e636
stonecast <arunkarki721@gmail.com>
2025-05-01 23:10:58 +0545

    updated dynamic route's page.tsx to have async access to params.

--- app/[movieId]/page.tsx
+++ app/[movieId]/page.tsx
@@ -1,6 +1,10 @@
 import MoviePage from "@/components/MoviePage";
 
-const MovieById = async ({ params }: { params: { movieId: string } }) => {
+const MovieById = async ({
+    params,
+}: {
+    params: Promise<{ movieId: string }>;
+}) => {
     const { movieId } = await params;
     return <MoviePage movieId={movieId} />;
 };
