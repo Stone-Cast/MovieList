d67bf3a9b87f830b67070bb10b659ea961df465c
stonecast <arunkarki721@gmail.com>
2025-05-01 23:15:39 +0545

    updated sectionName's page.tsx

--- app/searchResults/page.tsx
+++ app/searchResults/page.tsx
@@ -5,7 +5,6 @@ import { useMovieStore } from "@/store";
 
 const SerachResults = () => {
     const movies = useMovieStore((state) => state.searchResults);
-    const movieName = useMovieStore((state) => state.searchedMovieName);
 
     return <MovieGrid movies={movies} />;
 };
